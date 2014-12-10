e9309b072f07f618f80b314b6043e9a935bc5226
diegoavalos95 <diegoavalos95@outlook.com>
2014-12-10 00:52:27 -0600

    Updated script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 var nombre;
 
-nombre = prompt("¿Cuál es tu nombre, nene?", "Diego");
+nombre = prompt("¿Cuál es tu nombre, nene?");
+
+alert("Hola, " + nombre + " bebé, eres el worst y hueles a popo.");
